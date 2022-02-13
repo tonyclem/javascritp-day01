11a5c639547ebafcd30fb1551ac7a77757a1d69d
tonyclem <tonyclem001@gmail.com>
2022-02-13 11:45:38 +0100

    oncomment this exercise index day13

--- day13/index.js
+++ day13/index.js
@@ -4,33 +4,34 @@ const trafficLight = {
 };
 
 let cycle = 1;
+let cycle2 = 1;
 
-// while (cycle2 < 2) {
-//   const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
-//   console.log("The traffic light is on", currentState);
-//   trafficLight.stateIndex++;
-//   if (trafficLight.stateIndex === trafficLight.possibleStates.length) {
-//     trafficLight.stateIndex = 0;
-//     cycle2++;
-//   }
-// }
+while (cycle2 < 2) {
+  const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
+  console.log("The traffic light is on", currentState);
+  trafficLight.stateIndex++;
+  if (trafficLight.stateIndex === trafficLight.possibleStates.length) {
+    trafficLight.stateIndex = 0;
+    cycle2++;
+  }
+}
 
-// while (cycle < 2) {
-//   const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
-//   console.log("the traffic light is on", currentState);
-//   switch (currentState) {
-//     case "green":
-//       trafficLight.stateIndex = 1;
-//       break;
-//     case "orange":
-//       trafficLight.stateIndex = 2;
-//       break;
-//     default:
-//       trafficLight.stateIndex = 0;
-//       cycle++;
-//       break;
-//   }
-// }
+while (cycle < 2) {
+  const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
+  console.log("the traffic light is on", currentState);
+  switch (currentState) {
+    case "green":
+      trafficLight.stateIndex = 1;
+      break;
+    case "orange":
+      trafficLight.stateIndex = 2;
+      break;
+    default:
+      trafficLight.stateIndex = 0;
+      cycle++;
+      break;
+  }
+}
 
 //Question
 // TODO
