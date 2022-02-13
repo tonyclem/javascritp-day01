de20d956fab6d1661bc62203c3d81d095bedf38d
tonyclem <tonyclem001@gmail.com>
2022-02-13 15:21:56 +0100

    more exercise with extra example

---
+++ day15/exercise.js
@@ -0,0 +1,8 @@
+let a = [2, 3, 4, 5];
+let b = [2, 9, 4];
+
+function myFun(a, b) {
+  a + b;
+}
+
+myFun();
---
+++ day15/solutions.js
@@ -0,0 +1,78 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+("use strict");
+/**
+ * The `trafficLight` object is now no longer a global variable. Instead,
+ * it is defined in function `main()` and passed as a parameter to other
+ * functions, as and when needed.
+ */
+
+function getCurrentState(trafficLight) {
+  // TODO
+  // Should return the current state (i.e. colour) of the `trafficLight`
+  // object passed as a parameter.
+  return trafficLight.possibleStates[trafficLight.stateIndex];
+}
+// const trafficLight = {
+//   possibleStates: ["green", "orange", "red"],
+//   stateIndex: 2,
+// };
+
+function remainder(number, divider) {
+  const devision = Math.floor(number / divider);
+  return number - devision * divider;
+}
+
+function getNextStateIndex(trafficLight) {
+  const increased = trafficLight.stateIndex + 1;
+  // return remainder(increased, 3);
+  return increased % 3;
+  // TODO
+  // Return the index of the next state of the `trafficLight` such that:
+  // - if the color is green, it will turn to orange
+  // - if the color is orange, it will turn to red
+  // - if the color is red, it will turn to green
+}
+
+// This function loops for the number of seconds specified by the `secs`
+// parameter and then returns.
+// IMPORTANT: This is not the recommended way to implement 'waiting' in
+// JavaScript. You will learn better ways of doing this when you learn about
+// asynchronous code.
+function waitSync(secs) {
+  const start = Date.now();
+  while (Date.now() - start < secs * 1000) {
+    // nothing do to here
+  }
+}
+
+function main() {
+  const trafficLight = {
+    possibleStates: ["green", "orange", "red"],
+    stateIndex: 0,
+  };
+
+  for (let cycle = 0; cycle < 6; cycle++) {
+    const currentState = getCurrentState(trafficLight);
+    console.log(cycle, "The traffic light is now", currentState);
+
+    waitSync(1); // Wait a second before going to the next state
+    trafficLight.stateIndex = getNextStateIndex(trafficLight);
+  }
+}
+
+main();
+/**
+ * The output should be:
+0 The traffic light is now green
+1 The traffic light is now orange
+2 The traffic light is now red
+3 The traffic light is now green
+4 The traffic light is now orange
+5 The traffic light is now red
+*/
