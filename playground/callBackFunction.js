4242300b1934f80dcfb94538a9cf38373fac8610
tonyclem <tonyclem001@gmail.com>
2022-03-21 19:12:32 +0100

    added callback function

---
+++ playground/callBackFunction.js
@@ -0,0 +1,38 @@
+let students = [
+  { name: "cle", score: 80, location: "East" },
+  { name: "clement", score: 90, location: "West" },
+  { name: "sunday", score: 80, location: "North" },
+  { name: "junior", score: 50, location: "South" },
+  { name: "senior", score: 90, location: "West" },
+  { name: "secondary", score: 80, location: "East" },
+];
+
+let studentFun = function (obj, callBack) {
+  for (let i = 0; i < obj.length; i++) {
+    if (obj[i].location.toLowerCase() === "east") {
+      if (typeof callBack === "function") {
+        callBack(obj[i]);
+      }
+    }
+  }
+};
+
+studentFun(students, function (obj) {
+  if (obj.location === "East") {
+    console.log(obj.name + " good score");
+  }
+});
+
+let addAll = function () {
+  let total = 0;
+  count = 0;
+
+  studentFun(students, function (obj) {
+    total = total += obj.score;
+    count++;
+  });
+
+  console.log("add all score: " + total + " :get the iteration: " + count);
+};
+
+addAll();
