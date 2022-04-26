868e23551ccc3245164803953508844dc3f3b3bf
tonyclem <tonyclem001@gmail.com>
2022-04-26 15:58:50 +0200

    more lesson from FCC

--- 30-Day-JavaScript/find.js
+++ 30-Day-JavaScript/find.js
@@ -1,13 +1,192 @@
-// Find 
-const inventory = [
-    {name: 'apples', quantity: 2},
-    {name: 'bananas', quantity: 0},
-    {name: 'cherries', quantity: 5}
-  ];
-  
-  function isCherries(fruit) {
-    return fruit.name === 'cherries';
+// Find
+const inventoryName = [
+  { name: "apples", quantity: 2 },
+  { name: "bananas", quantity: 0 },
+  { name: "cherries", quantity: 5 },
+];
+
+function isCherries(fruit) {
+  return fruit.name === "cherries";
+}
+
+// console.log(inventoryName.find(isCherries)); //  {name: 'cherries', quantity: 5} // true
+
+// Exercise 2
+function filteredArray(arr, elem) {
+  let newArr = [];
+  arr.forEach((item) => {
+    if (item.indexOf(elem) === -1) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
+}
+
+// console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+// Exercise 3
+const eyeColor = "eyeColor";
+const tekkenCharacter = {
+  player: "Hwoarang",
+  fightingStyle: "Tae Kwon Doe",
+  human: true,
+};
+tekkenCharacter.favoriteGame = "Chess";
+tekkenCharacter["professional"] = "Computer Programmer";
+tekkenCharacter[eyeColor] = "Brown";
+// console.log(tekkenCharacter);
+
+// Exercise 4
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+function checkInventory(scannedItem) {
+  return foods[scannedItem];
+}
+let fruit = checkInventory("apples");
+// console.log(fruit);
+
+const getCurrentFood = (food) => {
+  return food;
+};
+
+let selectedFood = getCurrentFood("apples");
+let inventory = foods[selectedFood];
+// console.log(inventory);
+
+// Exercise 5
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+//   Finish writing the function so that it returns true if the object passed to it contains all four names, Alan, Jeff, Sarah and Ryan and returns false otherwise
+// users.hasOwnProperty('Alan');
+function isEveryoneHere(userObj) {
+    return (
+        userObj.hasOwnProperty("Alan") &&
+        userObj.hasOwnProperty("Jeff") &&
+        userObj.hasOwnProperty("Sarah") &&
+        userObj.hasOwnProperty("Ryan")
+    );
+}
+
+// console.log(isEveryoneHere(users));
+
+// Exercise 6
+const users2 = {
+    Alan: {
+      online: false
+    },
+    Jeff: {
+      online: true
+    },
+    Sarah: {
+      online: false
+    }
   }
   
-  console.log(inventory.find(isCherries)); //  {name: 'cherries', quantity: 5} // true
+//  count the users online
+  function countOnline(usersObj) {
+    // Only change code below this line
+    let count = 0;
+    for (let user in usersObj) {
+        if (usersObj[user].online === true) {
+            count++;
+        }
+    }
+    return count;
+
+    // Only change code above this line
+  }
   
+  console.log(countOnline(users2));
+
+//   Exercise 7
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+      username: 'kennethCodesAllDay',
+      joinDate: 'March 26, 2016',
+      organization: 'freeCodeCamp',
+      friends: [
+        'Sam',
+        'Kira',
+        'Tomo'
+      ],
+      location: {
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'USA'
+      }
+    }
+  };
+  
+  function addFriend(userObj, friend) {
+    // Only change code below this line
+    
+    userObj.data.friends.push(friend);
+
+
+    return userObj.data.friends;
+
+
+
+    // Only change code above this line
+  }
+  
+//   console.log(addFriend(user, 'Pete'));
+
+//  Exercise 8
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea,27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea,13);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
---
+++ 30-Day-JavaScript/freeCC.js
@@ -0,0 +1,118 @@
+// tabs is an array of titles of each site open within the window
+const Window = function(tabs) {
+    this.tabs = tabs; // We keep a record of the array inside the object
+  };
+  
+  // When you join two windows into one window
+  Window.prototype.join = function(otherWindow) {
+    this.tabs = this.tabs.concat(otherWindow.tabs);
+    return this;
+  };
+  
+  // When you open a new tab at the end
+  Window.prototype.tabOpen = function(tab) {
+    this.tabs.push('new tab'); // Let's open a new tab for now
+    return this;
+  };
+  
+  // When you close a tab
+  Window.prototype.tabClose = function(index) {
+  
+    // Only change code below this line
+
+    this.tabs.splice(index, 1);
+    return this;
+    };
+    // const tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
+    // const tabsAfterIndex = this.tabs.splice(index + 1); // Get the tabs after the tab
+  
+    // this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+  
+    // // Only change code above this line
+  
+    // return this;
+//    };
+  
+  // Let's create three browser windows
+  const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+  const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+  const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+  
+  // Now perform the tab opening, closing, and other operations
+  const finalTabs = socialWindow
+    .tabOpen() // Open a new tab for cat memes
+    .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+    .join(workWindow.tabClose(1).tabOpen());
+//   console.log(finalTabs.tabs);
+
+//  Exercise 8
+
+let fixedValue = 4;
+
+function incrementer() {
+    // Only change code below this line
+    return fixedValue += 1;
+    // Only change code above this line
+}
+// console.log(incrementer());
+
+// The global variable
+const bookList = ["The Hound of the Baskerville", "On The Electrodynamics of Moving Bodies", "Philosophic Naturalism Principal Mathematical", "Disquisitions Arithmetical"];
+
+function add(bookName) {
+    bookList.push(bookName);
+    return bookList;
+
+}
+console.log(add("A Brief History of Time"));
+
+function remove(bookName) {
+    const index = bookList.indexOf(bookName);
+    if (index > -1) {
+        bookList.splice(index, 1);
+    }
+}
+
+console.log(remove("The Hound of the Baskerville"));
+
+
+// Exercise 9
+/* This function should add a book to the list and return the list */
+// New parameters should come before bookName
+
+function add1(list, bookName) {
+  return [...list, bookName];
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+function remove1(list, bookName) {
+  return list.filter(book => book !== bookName);
+}
+
+var newBookList = add1(bookList, 'A Brief History of Time');
+var newerBookList = remove1(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove1(add1(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
+
+// Exercise 10
+function add2(arr, bookName) {
+    let newArr = [...arr]; // Copy the bookList array to a new array.
+    newArr.push(bookName); // Add bookName parameter to the end of the new array.
+    return newArr; // Return the new array.
+  }
+  
+  /* This function should remove a book from the list and return the list */
+  // New parameters should come before the bookName one
+  
+  // Add your code below this line
+  function remove2(arr, bookName) {
+    let newArr = [...arr]; // Copy the bookList array to a new array.
+    if (newArr.indexOf(bookName) >= 0) {
+      // Check whether the bookName parameter is in new array.
+      newArr.splice(newArr.indexOf(bookName), 1); // Remove the given parameter from the new array.
+      return newArr; // Return the new array.
+    }
+  }
