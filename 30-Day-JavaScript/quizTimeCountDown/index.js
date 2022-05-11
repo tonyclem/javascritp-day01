e6af5f7c06f404e961849aacd531493b6066d626
tonyclem <tonyclem001@gmail.com>
2022-05-11 16:57:09 +0200

    added time countdown break down

---
+++ 30-Day-JavaScript/quizTimeCountDown/index.js
@@ -0,0 +1,22 @@
+// Add A CountDown for the Quiz
+let time = 10;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+  let quizTimer = setInterval(function () {
+    if (quizTime <= 0) {
+      clearInterval(quizTimer);
+      showScores();
+    } else {
+      quizTime--;
+      let sec = Math.floor(quizTime % 60);
+      let min = Math.floor(quizTime / 60) % 60;
+      counting.innerHTML = `TIME: ${min} : ${sec}`;
+    }
+  }, 1000);
+}
+
+startCountdown();
